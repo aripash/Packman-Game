e061cc0d1c62021916519a58d63997ae96ae6dd1
Denis <aripash@gmail.com>
2018-12-17 22:13:00 +0200

    version 1.0.4

--- src/Game/PackMan.java
+++ src/Game/PackMan.java
@@ -1,5 +1,8 @@
 package Game;
 
+import java.util.Iterator;
+
+import Coords.MyCoords;
 import Geom.Point3D;
 
 public class PackMan {
@@ -49,5 +52,31 @@ public double getRadius() {
 }
 public Point3D getLastGps() {
 	return path.getPoints().get(path.getPoints().size()-1);
+}
+/**
+ * returns the two points the packman is between in the "time" second
+ * @param time the amout time the packman traveled
+ * @return array of points that the packman can be between
+ */
+public Point3D[] range(double time) {
+	Point3D[] ans=new Point3D[2];
+	MyCoords c=new MyCoords();
+	double searchDistance1=0;
+	double searchDistance2=0;
+	double distance=time*speed;
+	Iterator<Point3D> i=path.iterator();
+	Point3D pi=i.next();
+	while(i.hasNext()) {
+		Point3D temp=i.next();
+		searchDistance2=c.distance3d(pi, temp);
+		if(distance>=searchDistance1&&distance<=searchDistance2) {
+			ans[0]=pi;
+			ans[1]=temp;
+			return ans;
+		}
+		pi=temp;
+		searchDistance1=searchDistance2;
+	}
+	return null;
 }
 }
