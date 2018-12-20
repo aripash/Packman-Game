0d8f61752d2a38d9f5615a34b01b224e42ea5364
Denis <aripash@gmail.com>
2018-12-20 15:40:04 +0200

    version 1.0.7.1

--- src/File_format/Path2KML.java
+++ src/File_format/Path2KML.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 
+import Game.Fruit;
 import Game.Game;
 import Game.PackMan;
 
@@ -41,6 +42,14 @@ public class Path2KML {
 			int time=0;
 			while(time<game.lastTime()+2) {
 				sb.append("<Placemark>");
+				sb.append("<Style id=\"Packman\">");
+				sb.append("<IconStyle>");
+				sb.append("<scale>1.3</scale>");
+				sb.append("<Icon>");
+				sb.append("<href>http://maps.google.com/mapfiles/kml/pal3/icon49.png</href>"); 
+				sb.append("</Icon>"); 
+				sb.append("</IconStyle>");
+				sb.append("</Style>");
 				sb.append("<TimeStamp>");
 				sb.append("<when>");
 				if(time<10)sb.append("2018-08-16T22:00:0"+time);
@@ -58,6 +67,38 @@ public class Path2KML {
 				time++;
 			}
 		}
+		Iterator<Fruit> j=game.getFruits().iterator();
+		while(j.hasNext()) {
+			Fruit fi=j.next();
+				sb.append("<Placemark>");
+				sb.append("<Style id=\"Fruit\">");
+				sb.append("<IconStyle>");
+				sb.append("<scale>1.3</scale>");
+				sb.append("<Icon>");
+				sb.append("<href>http://maps.google.com/mapfiles/kml/paddle/F.png</href>"); 
+				sb.append("</Icon>"); 
+				sb.append("</IconStyle>");
+				sb.append("</Style>");
+				sb.append("<TimeSpan>");
+				sb.append("<begin>");
+				sb.append("2018-08-16T22:00:00");
+				sb.append("</begin>");
+				sb.append("<end>");
+				int time=(int) fi.getTime();
+				if(time<10)sb.append("2018-08-16T22:00:0"+time);
+				else sb.append("2018-08-16T22:00:"+time);
+				sb.append("</end>");
+				sb.append("</TimeSpan>");
+				sb.append("<Point>");
+				sb.append("<coordinates>");
+				sb.append(fi.getFruit().toString());
+				sb.append("</coordinates>");
+				sb.append("</Point>");
+				sb.append("</Placemark>");
+				pw.write(sb.toString());
+				sb.delete(0, sb.toString().length());
+			}
+		
 		sb.append("</Document>");
 		sb.append("</kml>");
 		pw.write(sb.toString());
